dccb1735b245a32f43ffbf7c5a73b6e8c259d6fa
Andre Walker <andre.walkermo@gmail.com>
2023-12-11 19:16:37 -0300

    fix: move parâmetros para classe Subscription

--- src/GuSubscriptionTrait.php
+++ src/GuSubscriptionTrait.php
@@ -10,22 +10,6 @@ use Potelo\GuPayment\Iugu\IuguSubscriptionDecorator;
  */
 trait GuSubscriptionTrait
 {
-    /**
-     * The attributes that are not mass assignable.
-     *
-     * @var array
-     */
-    protected $guarded = [];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'trial_ends_at', 'ends_at',
-        'created_at', 'updated_at',
-    ];
 
     protected $iuguSubscriptionModelIdColumn;
 
--- src/Subscription.php
+++ src/Subscription.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use GuSubscriptionTrait;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'trial_ends_at', 'ends_at',
+        'created_at', 'updated_at',
+    ];
 }
